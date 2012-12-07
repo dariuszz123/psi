b6c2f2c7032ca5edd202921221aac52e7c5fa95f
Darius Krištapavičius <dariuszz123@gmail.com>
2012-12-07 13:23:46 +0200

    some users work

--- WEB/application/controllers/main.php
+++ WEB/application/controllers/main.php
@@ -1,49 +1,39 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class Main extends MY_Controller {
-    public function __construct()
-       {
-            parent::__construct();
-       }
-    public function index() {        
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
         $this->template->write('title', 'VUMA - prisijungimas');
-        $data['msg'] = null;
+        
+        if($_POST) {
+            $success = $this->user_model->login($this->input->post('email'), $this->input->post('password'));
+            if($success === true) {
+                //redirect();
+            }else{
+                $this->session->set_flashdata('message_error', "Klaidingi prisijungimo duomenys");
+                redirect(current_url());
+            }
+        }
+        
+        $data = array();
         $this->template->write_view('center_content', 'login', $data);
         $this->template->render();
     }
+
     public function login() {
         $this->template->write('title', 'VUMA - prisijungimas');
-        $msg = NULL;
-	$session_id = $this->session->userdata('logged_in');
-        if ($session_id == true) {
-           
-        }
-        else {
-
-            $email = $this->security->xss_clean($this->input->post('email'));
-            $password = $this->security->xss_clean($this->input->post('password'));
-                if (empty($email) || empty($password) || !isset($_POST['submit']))
-                        $msg = "Tušti laukeliai";
-                else 
-                {
-                        $validate = $this->login_model->validateLogin($email, $password);
-                        if ($validate >=1)
-                        {
-                            $username = $this->login_model->getUserData($validate, 'nario_vardas, nario_levelis');
-                            $newdata = array(
-                                        'id' => $validate
-                                );
-                                $this->session->set_userdata($newdata);
-                                $msg = "Sveikiname sėkmingai prisijungus!";
-                        }
-                        else 
-                                $msg = "Klaida. Toks vartotojas neegzistuoja arba nėra aktyvuotas.";
-                }
-        }
-        $data['msg'] = $msg;
+        $data = array();
         $this->template->write_view('center_content', 'login', $data);
         $this->template->render();
     }
+
 }
 
 /* End of file welcome.php */
