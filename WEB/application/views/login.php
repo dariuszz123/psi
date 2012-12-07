c9d67f1b68ba7750b81224fffbb63f30e2fd4430
Darius Krištapavičius <dariuszz123@gmail.com>
2012-12-07 13:29:47 +0200

    fixes

--- WEB/application/views/login.php
+++ WEB/application/views/login.php
@@ -1,8 +1,7 @@
 <form class="form-signin" action="<?php echo base_url();?>" method="POST">
     <?php $this->load->view("errors/message_error"); ?>
->>>>>>> 1f17cbdf6c503db66262e37adb64297c5fbf8dc9
     <h2 class="form-signin-heading">Prisijungimas</h2>
-    <input type="text" class="input-block-level" placeholder="El. pa�tas" name="email">
+    <input type="text" class="input-block-level" placeholder="El. paštas" name="email">
     <input type="password" class="input-block-level" placeholder="Slaptažodis" name="password">
     <button class="btn btn-large btn-primary fl" type="submit" name="login">Prisijungti</button>
     <a href="<?php echo base_url();?>reg">
