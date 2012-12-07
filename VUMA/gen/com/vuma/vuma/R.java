7751703da29eee687ce1ee7303f0785a8cfd9e79
unknown <aurimas.sadauskas@gmail.com>
2012-12-07 15:09:12 +0200

    vuma naujinimai

--- VUMA/gen/com/vuma/vuma/R.java
+++ VUMA/gen/com/vuma/vuma/R.java
@@ -15,24 +15,41 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f070001;
-        public static final int button2=0x7f070002;
-        public static final int button3=0x7f070003;
-        public static final int button4=0x7f070004;
-        public static final int menu_login=0x7f070006;
-        public static final int menu_settings=0x7f070005;
-        public static final int textView2=0x7f070000;
+        public static final int add_title=0x7f070009;
+        public static final int button1=0x7f070004;
+        public static final int button2=0x7f070006;
+        public static final int button3=0x7f070007;
+        public static final int button4=0x7f070008;
+        public static final int checkBox1=0x7f070003;
+        public static final int editText1=0x7f070001;
+        public static final int editText2=0x7f070002;
+        public static final int menu_login=0x7f07000b;
+        public static final int menu_register=0x7f07000c;
+        public static final int menu_settings=0x7f07000a;
+        public static final int textView1=0x7f070000;
+        public static final int textView2=0x7f070005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int add_layout=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_login=0x7f060000;
+        public static final int activity_main=0x7f060001;
+        public static final int menu_login=0x7f060002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int hello_world=0x7f040009;
+        public static final int login_mail=0x7f04000c;
+        public static final int login_password=0x7f04000d;
+        public static final int login_remember=0x7f04000f;
+        public static final int login_text=0x7f04000e;
         public static final int menu_login=0x7f040003;
-        public static final int menu_settings=0x7f040002;
+        public static final int menu_register=0x7f040002;
+        public static final int menu_settings=0x7f04000a;
+        public static final int title_activity_login=0x7f04000b;
         public static final int title_activity_main=0x7f040004;
         public static final int vuma_bendravimas=0x7f040007;
         public static final int vuma_kontaktai=0x7f040008;
