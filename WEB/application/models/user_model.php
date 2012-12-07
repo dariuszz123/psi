b9cd547b9e59a9c888895479d8872aa01d0dcc64
Darius Krištapavičius <dariuszz123@gmail.com>
2012-12-07 12:04:16 +0200

    some user

--- WEB/application/models/user_model.php
+++ WEB/application/models/user_model.php
@@ -1,4 +1,7 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 /*
  * To change this template, choose Tools | Templates
@@ -10,77 +13,110 @@
  *
  * @author KD
  */
-class User_model extends CI_model{
+class User_model extends CI_model {
+
     public function __construct() {
         $this->load->database();
     }
-    public function getUserData($id)  {
+
+    public function get_user_data($id) {
         $query = $this->db->query("SELECT * FROM `users` WHERE `id` = '$id'");
-        return $query ->row_array();
+        return $query->row_array();
     }
-    public function IsExistEmail($email) {
+
+    public function is_user_exists($email) {
         $query = $this->db->query("SELECT `id` FROM `users` WHERE `nario_el_pastas` = '$email'");
         if ($query->num_rows() == 0) {
             return false;
-        }
-        else {
+        } else {
             return true;
         }
     }
-    public function validateLogin($email, $pass) {
-        $pass = base64_encode(pack('H*', sha1($pass)));
-        $query  = $this->db->query("SELECT `id` FROM `users` WHERE `user_email` = '$email' and `activated` = '1' and `user_password` = '$pass'");
+
+    public function encode_pass($pass) {
+        return base64_encode(pack('H*', sha1($pass)));
+    }
+
+    public function login($email, $pass) {
+        $query = $this->db->query("SELECT `id` FROM `users` WHERE `user_email` = '$email' and `activated` = '1' and `user_password` = '" . $this->encode_pass($pass) . "'");
         if ($query->num_rows() == 1) {
-            $date = date("Y-m-d H:i:s",time());
-            $this->db->query("UPDATE `users` SET `last_login` = '$date' WHERE `id` = '".$slap['id']."'");
-            return $slap['id'];
-        }
-        else {
-                return false;
-        }
-    }
-    public function addUser($data, $activation_code) {
-        $password = $data['password'];
-        $password2 = $data['passconf'];
-        $email = $this->apsaugos->sken($data['email']);
-        $user_type = $data['user_type'];
-        if ($password == $password2) {
-            $password_encoded = base64_encode(pack('H*', sha1($password)));
-            $date = date("Y-m-d H:i:s",time());
-            $this->db->query("INSERT INTO `users` 
-                            SET 
-                            `user_email` = '$email', 
-                            `user_password` = '$password_encoded',
-                            `nario_el_pastas` = '$email',
-                            `user_type` = '$user_type',
-                            `nario_reg_laikas` = '$date'
-                    ");
-                return true;
-        }
-        else {
-                return false;
-        }
-    }
-    public function activateUser($id) {
-        $query = $this->db->query("SELECT `id` FROM `users` WHERE `activation` = '$id'");
-        if ($query->num_rows() == 1)
-        {
-           $this->db->query("UPDATE `users` SET `activated` = '1' WHERE `id` = '$id'");
+            $user = $query->row_array();
+            $this->db->query("UPDATE `users` SET `last_login` = NOW() WHERE `id` = '" . $user['id'] . "'");
+            $this->session->set_userdata('user_id', $user['id']);
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
-    public function isLoggedIn() {
+
+    public function get_user_type($email) {
+
+        if (preg_match("#(.*?)@stud.(.*).vu.lt#is", $email) === true) {
+            return USER_STUDENT;
+        } else if (preg_match("#(.*?)@(.*).vu.lt#is", $email) === true) {
+            return USER_TEACHER;
+        }
+
+        return false;
+    }
+    
+    public function gen_activation_hash() {
+        return sha1(md5(hash('crc32', rand(1, 999))));
+    }
+
+
+    public function add_user($email, $password) {
+
+        if (strlen($password) < 6) {
+            $this->session->set_flashdata('message_error', "Slaptažodis per trumpas.");
+            return false;
+        }
+
+        $user_type = $this->get_user_type($email);
+
+        if ($user_type !== false) {
+            $this->session->set_flashdata('message_error', "Klaidingas el. pašto adresas.");
+            return false;
+        }
+
+        $this->db->query("INSERT INTO `users` 
+                            SET 
+                            `user_email` = '$email', 
+                            `user_password` = '".$this->encode_pass($password)."',
+                            `user_type` = '$user_type',
+                            `activation_hash` = '".$this->gen_activation_hash()."'
+        ");
+        
+        return true;
+    }
+    
+    public function is_valid_activation_hash($user_id, $hash) {
+        $user = $this->get_user_data($user_id);
+        if($user && $user['activation_hash'] !== null && $user['activation_hash'] == $hash) {
+            return true;
+        }
+        return false;
+    }
+    
+    public function activate_user($user_id, $hash) {
+        if($this->is_valid_activation_hash($user_id, $hash) === true) {
+            $this->db->query("UPDATE `users` SET `activated` = NULL WHERE `id` = '$user_id'");
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function is_loggedin() {
         $user_id = $this->session->userdata('user_id');
         if ($user_id) {
-            if ($this->getUserData($user_id)) {
+            if ($this->get_user_data($user_id)) {
                 return true;
             }
         }
         return false;
     }
+
 }
 
 ?>
