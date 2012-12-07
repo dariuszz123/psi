418cb16f8433f7b0831127b555bad3f331e032cf
Donce <admin@l2topai.eu>
2012-12-07 11:07:31 +0200

    login

--- WEB/application/controllers/reg.php
+++ WEB/application/controllers/reg.php
@@ -13,12 +13,12 @@
 class Reg  extends CI_Controller {
 	public function index()
 	{
-		$this->template->add_css('css/style.css');
-                $this->template->add_css('css/bootstrap.css');
-                $this->template->add_css('css/bootstrap-responsive.css');
-                $this->template->write('title', 'VUMA - registracija');
-                $this->template->write_view('center_content', 'reg');
-                $this->template->render();
+            $this->template->add_css('css/style.css');
+            $this->template->add_css('css/bootstrap.css');
+            $this->template->add_css('css/bootstrap-responsive.css');
+            $this->template->write('title', 'VUMA - registracija');
+            $this->template->write_view('center_content', 'reg');
+            $this->template->render();
 	}
         public function registration() {
             
