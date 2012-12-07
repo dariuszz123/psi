8e655f4dcd0b13d5aa5c1865386f9072e6d14132
Donce <admin@l2topai.eu>
2012-12-07 15:49:14 +0200

    blabla

--- WEB/application/controllers/user.php
+++ WEB/application/controllers/user.php
@@ -4,34 +4,68 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class User extends CI_Controller {
+    
+   private $meniu = array();
+    
+    public function __construct() {
+        parent::__construct();
+        if ($this->user_model->is_loggedin()) {
+            if ($this->user_model->user_have_role(USER_TEACHER)) {
+                $title = "Dėstytojams";
+                $this->meniu = array_merge($this->meniu, array (
+                    'name' => 'Vartotojo duomenys',
+                    'url' => '',
+                    'is_active' => true
+                ));
+                $this->meniu = array_merge($this->meniu, array (
+                    'name' => 'Pakeisti slaptažodį',
+                    'url' => '',
+                    'is_active' => ''
+                ));
+                $this->meniu = array_merge($this->meniu, array (
+                    'name' => 'Siųsti pranešimus',
+                    'url' => '',
+                    'is_active' => ''
+                ));
+                
+            }
+            else if ($this->user_model->user_have_role(USER_ADMIN)){
+                $title = "Administratoriams";
+            }
+            else {
+                redirect(base_url());
+            }
+        }
+    }
 
     public function index() {
-        if () {
-            
-        }
-        $this->template->write('title', 'VUMA - ');
-        
+        $data = array();
+        $data['nav'] = $this->meniu;
         $this->template->write_view('center_content', 'dash_board', $data);
         $this->template->render();
     }
 
-    public function viewUser() {
+    public function view_user() {
         
     }
 
-    public function delUser() {
+    public function del_user() {
         
     }
 
-    public function changeUserData() {
+    public function change_user_data() {
         
     }
 
-    public function makeAdmins() {
+    public function make_admins() {
         
     }
 
-    public function sendMail() {
+    public function send_message() {
+        
+    }
+    
+    public function all_users() {
         
     }
 
--- WEB/application/views/dash_board.php
+++ WEB/application/views/dash_board.php
@@ -1,15 +1,15 @@
 <div class="navbar navbar_vuma">
     <div class="navbar-inner">
         <ul class="nav">
-            <li class="active">
-                <a href="<?php echo base_url();?>">Vartotojo duomenys</a>    
-            </li>
-            <li>
-                <a href="<?php echo base_url();?>">Pakeisti slaptažodį</a>
-            </li>
-            <li>
-                <a href="<?php echo base_url();?>">Siųsti pranešimą</a>
-            </li>
+            <?php
+                foreach ($nav as $value) {
+                    ?>
+                    <li <?php echo ($value['is_active'])? 'class="active"':''; ?>
+                        <a href="<?php echo $value['url'];?>"><?php echo $value['name'];?></a>    
+                    </li>
+                    <?
+                }
+            ?>
         </ul> 
     </div>
 </div>
