c56a889b70857b996499ba0b3023924dfcc7575c
Donce <admin@l2topai.eu>
2012-12-07 17:11:38 +0200

    lba

--- WEB/application/views/dash_board.php
+++ WEB/application/views/dash_board.php
@@ -4,15 +4,15 @@
             <?php
                 foreach ($nav as $value) {
                     ?>
-                    <li <?php echo ($value['is_active'])? 'class="active"':''; ?>
+                    <li <?php echo ($value['is_active'])? 'class="active"':''; ?>>
                         <a href="<?php echo $value['url'];?>"><?php echo $value['name'];?></a>    
                     </li>
-                    <?
+                    <?php
                 }
             ?>
         </ul> 
     </div>
 </div>
-<div class="box_border" >
-       atvaizduojamas bla bal bal
+<div class="box_border">
+      <?php echo $content; ?>
 </div>
