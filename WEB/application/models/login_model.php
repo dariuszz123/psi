676fa1a6e44ffb9be1e61d34642f516406d42288
Donce <admin@l2topai.eu>
2012-12-07 08:27:05 +0200

    se

--- WEB/application/models/login_model.php
+++ WEB/application/models/login_model.php
@@ -12,7 +12,37 @@
  * @author KD
  */
 class Login_model extends CI_model{
-    //put your code here
+   public function __construct()
+	{
+            $this->load->database();
+	}
+   public function getUserData($id) 
+	{
+            $query = $this->db->query("SELECT * FROM `users` WHERE `id` = '$id'");
+            return $query ->row_array();
+	}
+    public function IsExistEmail($email) 
+    {
+        $query = $this->db->query("SELECT `id` FROM `users` WHERE `nario_el_pastas` = '$email'");
+        if ($query->num_rows() == 0) 
+            return 0;
+        else 
+            return 1;
+    }
+    public function validateLogin($email, $pass) 
+    {
+        $pass = base64_encode(pack('H*', sha1($pass)));
+        $query  = $this->db->query("SELECT `id`,`user_password` FROM `users` WHERE `user_email` = '$email' and `activated` = '1'");
+        $slap = $query ->row_array();
+        if ($query->num_rows() == 1 and $pass == $slap['user_password']) 
+        {
+            $date = date("Y-m-d H:i:s",time());
+            $this->db->query("UPDATE `users` SET `last_login` = '$date' WHERE `id` = '".$slap['id']."'");
+            return $slap['id'];
+        }
+        else
+                return 0;
+    }
 }
 
 ?>
--- WEB/application/models/reg_model.php
+++ WEB/application/models/reg_model.php
@@ -11,7 +11,44 @@
  * @author KD
  */
 class Reg_model extends CI_model {
-    //put your code here
+    public function __construct()
+    {
+        $this->load->database();
+    }
+    public function addUser($data, $activation_code)
+    {
+            $password = $data['password'];
+            $password2 = $data['passconf'];
+            $email = $this->apsaugos->sken($data['email']);
+            $user_type = $data['user_type'];
+            if ($password == $password2)
+            {
+                $password_encoded = base64_encode(pack('H*', sha1($password)));
+                $date = date("Y-m-d H:i:s",time());
+                $this->db->query("INSERT INTO `users` 
+                                SET 
+                                `user_email` = '$email', 
+                                `user_password` = '$password_encoded',
+                                `nario_el_pastas` = '$email',
+                                `user_type` = '$user_type',
+                                `nario_reg_laikas` = '$date'
+                        ");
+                    return true;
+            }
+            else 
+                    return false;
+    }
+    public function activateUser($id)
+    {
+            $query = $this->db->query("SELECT `id` FROM `users` WHERE `activation` = '$id'");
+            if ($query->num_rows() == 1)
+            {
+               $this->db->query("UPDATE `users` SET `activated` = '1' WHERE `id` = '$id'");
+                return true;
+            }
+            else 
+                return false;	
+    }
 }
 
 ?>
